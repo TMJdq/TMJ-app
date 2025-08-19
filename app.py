c861c5c42cf0232e600f936c47547195d1eb5b31
TMJdq <tara2575@naver.com>
2025-08-19 22:34:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -119,10 +119,13 @@ def generate_filled_pdf():
         "sleep_quality","sleep_tmd_relation","diagnosis_result"
     ]
 
-  
+    import textwrap
     values = {k: str(st.session_state.get(k, "")) for k in keys}
     values = {k: ("" if v == "선택 안 함" else v) for k, v in values.items()}
 
+    if "additional_habits" in values:
+    values["additional_habits"] = "\n".join(textwrap.wrap(values["additional_habits"], width=35))
+    
     for page in doc:
         placeholders_to_insert = {}
         for key, val in values.items():
